bdeea7d4dd382dd4716f5ffc394d8b3847d396f2
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-05-15 14:15:17 -0600

    8

--- index.js
+++ index.js
@@ -27,21 +27,28 @@ const mercedesModels = {
 let form = document.querySelector('.form');
 let brandSelect = document.getElementById('brandSelect');
 let modelAutoSelect = document.getElementById('modelAutoSelect');
+const result = document.querySelector('.result')
+const ownerQuantity = document.querySelector('.owner')
 
 let arrPriceCalc = [];
 
 const volumeInput = document.getElementById('volume');
-const newCheckbox = document.getElementById('new');
-const usedCheckbox = document.getElementById('used');
-const fewCheckbox = document.getElementById('few');
-const manyCheckbox = document.getElementById('many');
-const cashCheckbox = document.getElementById('cash');
-const cardCheckbox = document.getElementById('card');
-const companyCheckbox = document.getElementById('company');
+const petrolRadiobox = document.getElementById('petrol');
+const dieselRadiobox = document.getElementById('diesel');
+const gasRadiobox = document.getElementById('gas');
+const electricityRadiobox = document.getElementById('electricity');
+const newRadiobox = document.getElementById('new');
+const usedRadiobox = document.getElementById('used');
+const fewRadiobox = document.getElementById('few');
+const manyRadiobox = document.getElementById('many');
+const cashRadiobox = document.getElementById('cash');
+const cardRadiobox = document.getElementById('card');
+const companyRadiobox = document.getElementById('company');
 
 
 brandSelect.addEventListener('change',showModelAuto);
 modelAutoSelect.addEventListener('change',getPriceToArray);
+usedRadiobox.addEventListener('change',showOwner);
 
 function showModelAuto(event) {
     switch (event.target.value) {
@@ -76,4 +83,52 @@ function getObjModelRenderPage(obj, elementPage) {
 function getPriceToArray(event) {
     arrPriceCalc[0] = event.target.value;
     console.log(arrPriceCalc);
+}
+
+function showOwner(event) {
+    ownerQuantity.classList.remove('none')
+}
+
+function getPrice() {
+    if (volumeInput.value < 2) {
+        arrPriceCalc[1] = arrPriceCalc[0]*1
+    } else {
+        arrPriceCalc[1] = arrPriceCalc[0]*1.3
+    }
+
+    if (petrolRadiobox.checked = true) {
+        arrPriceCalc[2] = arrPriceCalc[1]*1
+    } else if (dieselRadiobox.checked = true) {
+        arrPriceCalc[2] = arrPriceCalc[1]*1.1
+    } else if (gasRadiobox.checked = true) {
+        arrPriceCalc[2] = arrPriceCalc[1]*1.2
+    } else if (electricityRadiobox.checked = true) {
+        arrPriceCalc[2] = arrPriceCalc[1]*1.3
+    } else {
+        alert('Выберите тип топлива!')
+    }
+
+    if (usedRadiobox.checked = true) {
+        if (fewRadiobox.checked = true) {
+            arrPriceCalc[3] = arrPriceCalc[2]*0.8
+        } else if (manyRadiobox.checked = true) {
+            arrPriceCalc[3] = arrPriceCalc[2]*0.6
+        } else {
+            alert('Выберите количество владельцев!')
+        }            
+    } else if (newRadiobox.checked = true) {
+        arrPriceCalc[3] = arrPriceCalc[2]*1
+    } else {
+        alert('Выберите состояние!')
+    }
+
+    if (cashRadiobox.checked = true) {
+        arrPriceCalc[4] = arrPriceCalc[3]*1
+    } else if ((cardRadiobox.checked = true)||(companyRadiobox.checked = true)) {
+        arrPriceCalc[4] = arrPriceCalc[3]*1.1
+    } else {
+        alert('Выберите способ оплаты!')
+    }
+
+
 }
