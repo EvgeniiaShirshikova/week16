4bfe6c40df533d8c72977b422110daec760503e2
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-05-15 15:19:13 -0600

    final

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+//объекты с базовой ценой по моделям
+
 const renaultModels = {
     'Logan': 10000,
     'Stepway': 20000,
@@ -24,6 +27,9 @@ const mercedesModels = {
     'S-class': 120000
 }
 
+
+// элементы разметки
+
 let form = document.querySelector('.form');
 let brandSelect = document.getElementById('brandSelect');
 let modelAutoSelect = document.getElementById('modelAutoSelect');
@@ -32,7 +38,11 @@ const button = document.querySelector('.button');
 const result = document.querySelector('.result');
 const cleanButton = document.querySelector('.clean');
 
+// массивы
+
 let arrPriceCalc = [];
+
+// инпуты и радиокнопки
 
 const volumeInput = document.getElementById('volume');
 const petrolRadiobox = document.getElementById('petrol');
@@ -47,6 +57,7 @@ const cashRadiobox = document.getElementById('cash');
 const cardRadiobox = document.getElementById('card');
 const companyRadiobox = document.getElementById('company');
 
+// ивентлистенеры
 
 brandSelect.addEventListener('change',showModelAuto);
 modelAutoSelect.addEventListener('change',getPriceToArray);
@@ -54,6 +65,8 @@ usedRadiobox.addEventListener('change',showOwner);
 button.addEventListener('click',showResultPrice);
 cleanButton.addEventListener('click',cleanForm);
 
+
+// функция для показа списка моделей авто в зависмости от выбранного бренда
 function showModelAuto(event) {
     switch (event.target.value) {
         case 'Renault':
@@ -68,38 +81,45 @@ function showModelAuto(event) {
         case 'Mercedes':
             getObjModelRenderPage(mercedesModels,modelAutoSelect)
             break
-        default:
-            arrPriceCalc
-            break
     }    
 }
 
+
+//функция для заполнения селекта с моделями
 function getObjModelRenderPage(obj, elementPage) {
     elementPage.innerHTML = '<option hidden>модель</option>';
     for(key in obj){
         const option = document.createElement('option');
         option.value = obj[key];
-        option.innerText = key;
+        option.textContent = key;
         elementPage.append(option);
     }
 }
 
+
+//функция для добавления базовой цены в массив в качестве значения с индексом 0
 function getPriceToArray(event) {
     arrPriceCalc[0] = event.target.value;
 }
 
+
+//функция для показа блока радиокнопок с кол-вом владельцев, если авто подержанное
 function showOwner(event) {
     ownerQuantity.classList.remove('none')
 }
 
+
+//функция для добавления цены с коэфициентом в зав-ти от выбранных опций в массив. Последнее значение - итоговая цена.
 function showResultPrice(event) {
     event.preventDefault();
+    //в зав-ти от объема двигателя
     if (volumeInput.value < 2) {
         arrPriceCalc[1] = arrPriceCalc[0]*1
     } else {
         arrPriceCalc[1] = arrPriceCalc[0]*1.3
     }
 
+    //в зав-ти от типа топлива
     if (petrolRadiobox.checked) {
         arrPriceCalc[2] = arrPriceCalc[1]*1
     } else if (dieselRadiobox.checked) {
@@ -112,6 +132,7 @@ function showResultPrice(event) {
         alert('Выберите тип топлива!')
     }
 
+    //в зав-ти от состояния и кол-ва владельцев
     if (usedRadiobox.checked) {
         if (fewRadiobox.checked) {
             arrPriceCalc[3] = arrPriceCalc[2]*0.8
@@ -126,6 +147,7 @@ function showResultPrice(event) {
         alert('Выберите состояние!')
     }
 
+    //в зав-ти от способа оплаты
     if (cashRadiobox.checked) {
         arrPriceCalc[4] = arrPriceCalc[3]*1
     } else if ((cardRadiobox.checked)||(companyRadiobox.checked)) {
@@ -134,12 +156,12 @@ function showResultPrice(event) {
         alert('Выберите способ оплаты!')
     }
 
-
-    console.log(arrPriceCalc);
-
+    //выводим итог в див под кнопкой рассчитать
     result.textContent = Math.round(arrPriceCalc[4]);
 }
 
+
+//функция для очистки калькулятора
 function cleanForm(event) {
     form.reset();
     result.textContent = '';
